608ee2617933657a1d4696f6a9be2c3bdbad21d7
saneks222 <66480963+saneks222@users.noreply.github.com>
2021-09-27 16:00:02 +0500

    Добавьте файлы проекта.

---
+++ SinxPaternLab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SinxPaternLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("V1.0");
+
+            Tintset tintset1 = new Tintset(4);
+            Tintset tintset2 = new Tintset(4);
+
+            tintset1.input();
+            tintset2.input();
+
+            tintset1.watch();
+            tintset2.watch();
+
+           Console.WriteLine(tintset1.Equals(tintset2));
+
+            tintset1.SumOtherNumber(tintset2);
+
+            tintset1.watch();
+        }
+    }
+}
---
+++ SinxPaternLab2/Tintset.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinxPaternLab2
+{
+    class Tintset
+    {
+        //ссылочный тип данных приобращение не происходит копирования фактически является самим множеством и ссылкой на множество 
+        private List<int> numbers = new List<int>();
+        int MaxCount;
+        
+
+        //явный конструктор без параметров создается пустое множество
+        public Tintset() 
+        {
+            MaxCount = numbers.Count;
+        }
+
+        //конструктор 
+        public Tintset(int count) 
+        {
+            MaxCount = count;
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++) 
+            {
+                numbers.Add(random.Next());
+            }
+        }
+
+        //конструктор копирования
+        public Tintset(Tintset T) 
+        {
+            this.numbers = T.numbers;
+        }
+
+        public void input()
+        {
+            Console.WriteLine("Введите "+MaxCount+" Чисел");
+
+            for (int i = 0; i < MaxCount; i++) 
+            {
+                try { numbers[i] = int.Parse(Console.ReadLine()); }
+                catch(Exception e) 
+                { 
+                    Console.WriteLine(e.Message + " введено не число попробуй снова"); 
+                    i--; 
+                }
+            }
+        }
+
+        public void watch() 
+        {
+            foreach (var i in numbers) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Колличестов элементов = " + MaxCount);
+        }
+
+        public bool Equals(Tintset T) 
+        {
+            var numbers1 = this.numbers;
+            var numbers2 = T.numbers;
+
+            //если колличество элементов не совпадает 
+            if (numbers1.Count != numbers2.Count) 
+            {
+                return false;
+            }
+
+            numbers1.Sort();
+            numbers2.Sort();
+
+            for (int i = 0; i < numbers1.Count; i++) 
+            {
+                if (numbers1[i] != numbers2[i]) { return false; }
+            }
+
+            return true;
+        }
+
+        public void SumOtherNumber(Tintset T) 
+        {
+            this.MaxCount += T.MaxCount;
+            this.numbers.AddRange(T.numbers);
+        }
+    }
+}
